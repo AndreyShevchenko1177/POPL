5d87e85f6c6e768e244d7da72b626e10fdd8c922
davidcole21 <claude.r80k@gmail.com>
2021-03-04 14:47:28 -0500

    changed get subscription endpoint

--- src/pages/billing/stripeConfig.js
+++ src/pages/billing/stripeConfig.js
@@ -1,7 +1,6 @@
 const config = {
   stripePk: "pk_test_51HGjVfCcIG0MPJSGSyEcBP3PyICKdRKpXMC1TP4r08nXQ6diqfdIN4Uw3O5c1vdgEqc7deyecbDYde46HdRMdVtt00trU2DOSP",
-  // getSessionIdUrl: "https://stripe-example.hopto.org/api/subscription",
-  getSessionIdUrl: "http://localhost:8000/api/subscription",
+  getSessionIdUrl: "https://stripe-example.hopto.org/api/subscription",
 };
 
 export default config;
