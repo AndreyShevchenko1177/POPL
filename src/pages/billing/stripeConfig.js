d493f2804bd8276395158df5e601f0793242660e
davidcole21 <david.cole10m@gmail.com>
2021-03-23 04:36:28 -0400

    changed stripe key

--- src/pages/billing/stripeConfig.js
+++ src/pages/billing/stripeConfig.js
@@ -1,5 +1,5 @@
 const config = {
-  stripePk: "pk_test_51ISRsnJqkGKmOFO6YO8opYbn524kaQNKUBErq2EHeWYtwB4NQSnR54gv8dIxKO21rhU0pPGa9X4KZlz5LI3WflAL00E6CtMenM",
+  stripePk: "pk_test_51ISRsnJqkGKmOFO6Ifj4pCqqKRR4GGKAW2Jz4XQWUdYl7EoAwTj9qPPdCj26kUCS9uO7whxCKNeqaujPUzgzQCnd00C6uN5riN",
   // getSessionIdUrl: "https://stripe-example.hopto.org/api/subscription",
   getSessionIdUrl: "http://localhost:8000/api/subscription",
 };
