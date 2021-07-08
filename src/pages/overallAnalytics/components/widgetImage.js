24f5147d60d274773f413b05946c62200bac7a1e
davidcole21 <david.cole10m@gmail.com>
2021-07-08 20:37:26 +0300

    fixed company image in widget heading

--- src/pages/overallAnalytics/components/widgetImage.js
+++ src/pages/overallAnalytics/components/widgetImage.js
@@ -13,10 +13,11 @@ function WidgetImage({
         <div key={key} style={{
           width, height, marginBottom: 10, marginRight: 10, minWidth: 25,
         }}>
+          {console.log(el.image)}
           {el.image || (generalSettingsData && generalSettingsData[3])
             ? <img style={{
               width: "100%", height: "100%", borderRadius: "50%", objectFit: "cover",
-            }} src={`${el.image ? process.env.REACT_APP_BASE_FIREBASE_PHOTOS_URL + el.image : `${process.env.REACT_APP_BASE_FIREBASE_CUSTOM_ICON}${generalSettingsData[3]}`}?alt=media`} alt='avatar' />
+            }} src={`${el.image ? process.env.REACT_APP_BASE_FIREBASE_PHOTOS_URL + el.image : `${process.env.REACT_APP_BASE_FIREBASE_LOGOS_URL}${generalSettingsData[3]}`}?alt=media`} alt='avatar' />
             : <div style={{
               width: "100%", height: "100%", borderRadius: "50%", boxShadow: "0px 0px 8px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)", backgroundColor: `${generalSettingsData && generalSettingsData[1]}`,
             }}>  </div>}
